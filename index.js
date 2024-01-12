4a2584df2f696fbe470d0d3eca9ea2cf25c94c58
Martin <mfkutz@gmail.com>
2024-01-12 20:47:47 -0300

    ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* const express = require('express')
+const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -20,66 +20,66 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`port running in http://localhost:${port}`)
-}) */
+})
 
 
 
 
 
 ////////////////////////////
-const express = require('express')
-const ProductManager = require('./ProductManager') //import OK
-const app = express()
-const port = process.env.PORT || 8080
+// const express = require('express')
+// const ProductManager = require('./ProductManager') //import OK
+// const app = express()
+// const port = process.env.PORT || 8080
 
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
-const productManager = new ProductManager('./products.json')
-productManager.initialize()
-    .then(() => {
-        app.listen(port, console.log(`Server online - Port ${port}`))
-    })
-    .catch(error => console.error(error))
+// const productManager = new ProductManager('./products.json')
+// productManager.initialize()
+//     .then(() => {
+//         app.listen(port, console.log(`Server online - Port ${port}`))
+//     })
+//     .catch(error => console.error(error))
 
-app.get('/products', async (req, res) => {
-    try {
-        const limit = parseInt(req.query.limit, 10) || undefined;
-        const allProducts = await productManager.getProducts();
+// app.get('/products', async (req, res) => {
+//     try {
+//         const limit = parseInt(req.query.limit, 10) || undefined;
+//         const allProducts = await productManager.getProducts();
 
-        if (limit) {
-            const limitedProducts = allProducts.slice(0, limit);
-            res.json(limitedProducts);
-        } else {
-            res.json(allProducts);
-        }
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-})
+//         if (limit) {
+//             const limitedProducts = allProducts.slice(0, limit);
+//             res.json(limitedProducts);
+//         } else {
+//             res.json(allProducts);
+//         }
+//     } catch (error) {
+//         res.status(500).send('Internal Server Error');
+//     }
+// })
 
-app.get('/', (req, res) => {
-    const welcome = `
-    <html>
-        <body>
-            <h1>Welcome to server</h1>
-        </body>
-    </html>
-    `
-    res.send(welcome)
-})
+// app.get('/', (req, res) => {
+//     const welcome = `
+//     <html>
+//         <body>
+//             <h1>Welcome to server</h1>
+//         </body>
+//     </html>
+//     `
+//     res.send(welcome)
+// })
 
-app.get('/products/:id', async (req, res) => {
-    try {
-        let isId = parseInt(req.params.id, 10)
-        const foundProduct = await productManager.getProductBytId(isId)
+// app.get('/products/:id', async (req, res) => {
+//     try {
+//         let isId = parseInt(req.params.id, 10)
+//         const foundProduct = await productManager.getProductBytId(isId)
 
-        if (foundProduct !== 'Not found') {
-            res.json(foundProduct)
-        } else {
-            res.send(`Product with ID ${req.params.id} not found`)
-        }
-    } catch (error) {
-        res.send(`Ups...Something went wrong`)
-    }
-})
+//         if (foundProduct !== 'Not found') {
+//             res.json(foundProduct)
+//         } else {
+//             res.send(`Product with ID ${req.params.id} not found`)
+//         }
+//     } catch (error) {
+//         res.send(`Ups...Something went wrong`)
+//     }
+// })
 
